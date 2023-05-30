da56f22595f9e69020aaca38c6b3f951418dce2f
massone99 <lorenzomassone@gmail.com>
2023-05-30 20:13:47 +0200

    Improved aspect of EditRecipePage.kt

--- app/src/main/java/com/lucertola/ratatouille/ui/pages/EditRecipePage.kt
+++ app/src/main/java/com/lucertola/ratatouille/ui/pages/EditRecipePage.kt
@@ -9,18 +9,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -85,7 +80,8 @@ object EditRecipePage {
                                         Recipe(
                                             name,
                                             description,
-                                            ingredients.split("\n").map { it.trim() }.filter { it.isNotEmpty() })
+                                            ingredients.split("\n").map { it.trim() }
+                                                .filter { it.isNotEmpty() })
                                     )
                                 }) {
                                     Text("Confirm")
