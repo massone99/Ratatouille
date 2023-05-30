820d4f602a2b21c2d59b6e537d31d64e24bbf553
massone99 <lorenzomassone@gmail.com>
2023-05-30 11:02:34 +0200

    Basic navigation and recipe management

--- app/src/main/java/com/lucertola/ratatouille/data/Recipe.kt
+++ app/src/main/java/com/lucertola/ratatouille/data/Recipe.kt
@@ -1,8 +1,3 @@
 package com.lucertola.ratatouille.data
 
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
-data class Recipe(var name: String, val description: String, val ingredients: List<String>) :
-    Parcelable
+data class Recipe(var name: String, val description: String, val ingredients: List<String>)
